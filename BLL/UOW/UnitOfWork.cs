f6a8d9483fa7d7f29012bd524887e9529bf7ac46
Руслан Сулейманов <ruslansk88@gmail.com>
2023-04-29 10:54:07 +0600

    Добавьте файлы проекта.

---
+++ BLL/UOW/UnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿
+namespace BLL.UOW;
+
+public class UnitOfWork
+{
+    private AppDbContext context;
+
+    public UnitOfWork(AppDbContext _context)
+    {
+        context = _context;
+    }
+}
---
+++ DAL/Datas/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Datas;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+    //public DbSet
+}
